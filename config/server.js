28fdcd79b4d221e5d27c19dca0a03f3f71f8d17e
Saugat Poudel <poudelsaugat45@gmail.com>
2020-09-07 10:13:05 +0545

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cc122d666c4cbc32cdf663e3b18420f2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0789d351-2a9f-4393-b447-b3e2b103a3fa'
+};
